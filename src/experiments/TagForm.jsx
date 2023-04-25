696e1ac31b8641985d4639227ff6eb17f0223e3f
Jason Boxman <jasonb@edseek.com>
2023-04-24 20:21:12 -0400

    Add experimental tag form

--- src/experiments/TagForm.jsx
+++ src/experiments/TagForm.jsx
@@ -1,59 +1,75 @@
-import { TextInput, Button, Group, Box, Autocomplete, ScrollArea } from '@mantine/core';
+import {
+  TextInput,
+  Button,
+  Group,
+  Box,
+  Select,
+  ScrollArea,
+} from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { forwardRef } from "react";
+//import { forwardRef } from 'react';
 
 const TagForm = (props = {}) => {
-
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    (async function() {
-      setData(await import('./tags-flat.json').then(
-        (module) => module.default
-      ));
+    (async function () {
+      setData(
+        await import('./tags-flat.json').then((module) => module.default)
+      );
     })();
-  }, [])
+  }, []);
 
   const form = useForm({
     initialValues: {
-      tagName: ''
-    }
+      tagName: '',
+      parentId: '',
+    },
   });
 
   const AutoCompleteItem = ({ value, ...others }, ref) => {
     return (
-    <div ref={ref} {...others}>
-          <Text size="xs" color="dimmed">
-            {description}
-          </Text>
-    </div>)
-  }
+      <div ref={ref} {...others}>
+        <Text size="xs" color="dimmed">
+          {description}
+        </Text>
+      </div>
+    );
+  };
 
   return (
     <Box maw={300} mx="auto">
-    <form>
-      <Group>
-        <TextInput label="Tag" withAsterisk {...form.getInputProps('tagName')} />
-        <Autocomplete label="Parent" dropdownComponent={forwardRef((props, ref) => (
-            <ScrollArea viewportRef={ref} {...props} />
-          ))}
-          withAsterisk data={data} limit={15} maxDropdownHeight='3rem' />
-      </Group>
+      <form>
+        <Group>
+          <TextInput
+            label="Tag"
+            withAsterisk
+            {...form.getInputProps('tagName')}
+          />
+          <Select
+            label="Parent"
+            withAsterisk
+            searchable
+            clearable
+            data={data}
+            maxDropdownHeight={100}
+            {...form.getInputProps('parentId')}
+          />
+        </Group>
 
-      <Group sx={{
-        padding: '1rem 0 0 0'
-      }}>
-        <Button>
-          Create
-        </Button>
-        <Button>
-          Cancel
-        </Button>
-      </Group>
-    </form>
+        <Group
+          sx={{
+            padding: '1rem 0 0 0',
+          }}
+        >
+          <Button>Create</Button>
+          <Button>Cancel</Button>
+        </Group>
+      </form>
     </Box>
   );
 };
+
 export default TagForm;
